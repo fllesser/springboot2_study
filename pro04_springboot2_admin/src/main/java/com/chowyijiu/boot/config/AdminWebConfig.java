a165d685a86c2525e2d88ec8eb7c41c985e9ccbc
YiJiuChow <19550518zrf@gmail.com>
2022-10-09 22:15:35 +0800

    自定义原理

--- pro04_springboot2_admin/src/main/java/com/chowyijiu/boot/config/AdminWebConfig.java
+++ pro04_springboot2_admin/src/main/java/com/chowyijiu/boot/config/AdminWebConfig.java
@@ -1,14 +1,25 @@
 package com.chowyijiu.boot.config;
 
 import com.chowyijiu.boot.intercepter.LoginInterceptor;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcRegistrations;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 /**
  * 1. 编写一个拦截器实现HandlerInterceptor接口
  * 2. 拦截器注册到容器中(实现WebMvcConfigurer的addInterceptor)
+ * 3. 指定拦截规则(如果是拦截所有, 静态资源也会被拦截
+ *
+ * @EnableWebMvc 全面接管
+ *  1.静态资源, 视图解析器, 欢迎页, 全部失效
  */
+
+@EnableWebMvc
 @Configuration
 public class AdminWebConfig implements WebMvcConfigurer {
 
@@ -16,6 +27,26 @@ public class AdminWebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoginInterceptor())
                 .addPathPatterns("/**") //所有请求都会拦截, 包括静态资源
-                .excludePathPatterns("/", "/login", "/css/**", "/js/**", "/fonts/**", "/images/**");  // 放行请求
+                .excludePathPatterns("/", "/login", "/css/**", "/js/**", "/fonts/**", "/images/**", "/aa/**");  // 放行请求
     }
+
+    /**
+     * 静态资源
+     * @param registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/aa/**")
+                .addResourceLocations("classpath:/static/");
+    }
+
+//    @Bean
+//    public WebMvcRegistrations webMvcRegistrations() {
+//        return new WebMvcRegistrations() {
+//            @Override
+//            public RequestMappingHandlerMapping getRequestMappingHandlerMapping() {
+//                return WebMvcRegistrations.super.getRequestMappingHandlerMapping();
+//            }
+//        };
+//    }
 }
--- pro04_springboot2_admin/src/test/java/com/chowyijiu/boot/pro04_springboot2_admin/Pro04Springboot2AdminApplicationTests.java
+++ pro04_springboot2_admin/src/test/java/com/chowyijiu/boot/pro04_springboot2_admin/Pro04Springboot2AdminApplicationTests.java
@@ -1,13 +1,23 @@
 package com.chowyijiu.boot.pro04_springboot2_admin;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
+@Slf4j
 @SpringBootTest
 class Pro04Springboot2AdminApplicationTests {
 
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
     @Test
     void contextLoads() {
+        Long aLong = jdbcTemplate.queryForObject("SELECT count(*) FROM  t_emp", Long.class);
+        log.info("总记录数 " + aLong);
     }
+
 
 }
