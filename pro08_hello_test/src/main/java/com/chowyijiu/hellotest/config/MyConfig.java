88ffcd75be7a954f3ac97e5de39a30562d241b2a
YiJiuChow <19550518zrf@gmail.com>
2022-10-14 23:22:56 +0800

    10-14

--- pro08_hello_test/src/main/java/com/chowyijiu/hellotest/config/MyConfig.java
+++ pro08_hello_test/src/main/java/com/chowyijiu/hellotest/config/MyConfig.java
@@ -9,8 +9,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableConfigurationProperties({HelloProperties.class})
 public class MyConfig {
-
-
     @Bean
     public HelloService helloService() {
         return new HelloService();
