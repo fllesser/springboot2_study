86f0a34f5ceb488571a2797d182ba0a76b3d9858
YiJiuChow <19550518zrf@gmail.com>
2022-10-17 19:14:11 +0800

    ssm项目使用Mybatis-Plus简化

--- pro09_mybatis_plus_study/src/main/java/com/chowyijiu/boot/mbp/pojo/Emp.java
+++ pro09_mybatis_plus_study/src/main/java/com/chowyijiu/boot/mbp/pojo/Emp.java
@@ -1,10 +1,13 @@
 package com.chowyijiu.boot.mbp.pojo;
 
+
 import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @TableName("t_emp")
+@AllArgsConstructor
 public class Emp {
     private Integer empId;
     private String empName;
--- pro09_mybatis_plus_study/src/test/java/com/chowyijiu/boot/mbp/Pro09MybatisPlusStudyApplicationTests.java
+++ pro09_mybatis_plus_study/src/test/java/com/chowyijiu/boot/mbp/Pro09MybatisPlusStudyApplicationTests.java
@@ -1,15 +1,23 @@
 package com.chowyijiu.boot.mbp;
 
+
+import com.baomidou.mybatisplus.core.conditions.AbstractLambdaWrapper;
+import com.baomidou.mybatisplus.core.conditions.AbstractWrapper;
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.chowyijiu.boot.mbp.mapper.EmpMapper;
 import com.chowyijiu.boot.mbp.mapper.UserMapper;
 import com.chowyijiu.boot.mbp.pojo.Emp;
 import com.chowyijiu.boot.mbp.pojo.User;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 @SpringBootTest
 class Pro09MybatisPlusStudyApplicationTests {
@@ -23,15 +31,15 @@ class Pro09MybatisPlusStudyApplicationTests {
     void contextLoads() {
         System.out.println("====== Select Method =====");
         List<User> users = userMapper.selectList(null);
-        Assertions.assertEquals(5, users.size());
+        assertEquals(5, users.size());
         users.forEach(System.out::println);
     }
 
     @Test
     void test1() {
-        List<Emp> emps = empMapper.selectList(null);
+        List<Emp> emps = empMapper.selectList(new QueryWrapper<Emp>().in("age", 99));
         emps.forEach(System.out::println);
+        empMapper.insert(new Emp(null, "chowyijiu", 99, 0, "19550518zrf@gmail.com"));
+
     }
-
-
 }
