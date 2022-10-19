da54705816298dd11c88a3fa0b01da516f37a675
YiJiuChow <19550518zrf@gmail.com>
2022-10-19 23:42:17 +0800

    链表反转

---
+++ algorithm/src/linkedtable_reverse/Solution.java
@@ -0,0 +1,23 @@
+package linkedtable_reverse;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Node node1 = new Node(1);
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+        Node node4 = new Node(4);
+        Node node5 = new Node(5);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        reverse1(node1);
+        System.out.println(node1);
+
+    }
+
+    public static void reverse1(Node head) {
+
+    }
+}
