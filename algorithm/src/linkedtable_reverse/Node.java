b6498ae5d8ce5db72fa31c62e8248810ae96090a
YiJiuChow <19550518zrf@gmail.com>
2022-10-19 23:42:17 +0800

    链表反转

---
+++ algorithm/src/linkedtable_reverse/Node.java
@@ -0,0 +1,23 @@
+package linkedtable_reverse;
+
+public class Node {
+
+    public int val;
+    public Node next;
+
+    public Node() {
+
+    }
+
+    public Node(int val) {
+        this.val = val;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "val=" + val +
+                ", next=" + next +
+                '}';
+    }
+}
