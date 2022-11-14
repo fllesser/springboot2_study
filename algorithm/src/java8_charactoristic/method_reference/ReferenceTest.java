0db18b86e2dc7884b6473e70678c70fdd0a4ad5e
YiJiuChow <19550518zrf@gmail.com>
2022-11-14 18:30:13 +0800

    java 8 特性复习 Method Reference 摸鱼

---
+++ algorithm/src/java8_charactoristic/method_reference/ReferenceTest.java
@@ -0,0 +1,14 @@
+package java8_charactoristic.method_reference;
+
+import java.util.ArrayList;
+
+public class ReferenceTest {
+
+    public static void main(String[] args) {
+        ArrayList<String> strings = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            strings.add("Test Reference " + i);
+        }
+        strings.stream().limit(5).forEach(System.out::println);
+    }
+}
