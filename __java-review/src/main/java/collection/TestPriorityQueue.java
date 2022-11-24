e7118143becdad25553dff99b53855402da84e0e
YiJiuChow <19550518zrf@gmail.com>
2022-11-24 23:11:51 +0800

    PriorityQueue 优先队列, 其与 Queue 的区别在于元素出队顺序是与优先级相关的，即总是优先级最高的元素先出队,值越小,优先级越高, 可以接收一个 Comparator 作为构造参数，从而来自定义元素优先级的先后。

--- __java-review/src/main/java/collection/TestPriorityQueue.java
+++ __java-review/src/main/java/collection/TestPriorityQueue.java
@@ -4,17 +4,36 @@ import java.util.PriorityQueue;
 
 public class TestPriorityQueue {
 
+    //可以接收一个 Comparator 作为构造参数，从而来自定义元素优先级的先后。
     public static void main(String[] args) {
-        PriorityQueue<Integer> integers = new PriorityQueue<>();
-        integers.add(5);
-        integers.add(2);
-        integers.add(3);
-        integers.add(4);
-        integers.add(1);
-        System.out.println(integers.poll());
-        System.out.println(integers.poll());
-        System.out.println(integers.poll());
-        System.out.println(integers.poll());
-        System.out.println(integers.poll());
+        PriorityQueue<Person> peoples = new PriorityQueue<>((p1, p2) -> p2.age.compareTo(p1.age));
+        peoples.add(new Person("xiaohong", 20));
+        peoples.add(new Person("xiaoming", 21));
+        peoples.add(new Person("xiaoxixi", 24));
+        peoples.add(new Person("xiaohaha", 18));
+        peoples.add(new Person("xiaonainiu", 17));
+        System.out.println(peoples.poll());
+        System.out.println(peoples.poll());
+        System.out.println(peoples.poll());
+        System.out.println(peoples.poll());
+        System.out.println(peoples.poll());
+    }
+}
+
+class Person {
+    String name;
+    Integer age;
+
+    public Person(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
