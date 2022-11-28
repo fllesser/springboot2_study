bc9fa5a50851cacf65d6116e5622b95a5864bbf2
YiJiuChow <19550518zrf@gmail.com>
2022-11-28 09:45:24 +0800

    int to Roman 算法题

--- algorithm/src/main/java/_017_intoroman/Solution.java
+++ algorithm/src/main/java/_017_intoroman/Solution.java
@@ -6,7 +6,7 @@ public class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.intToRoman(1994));
+        System.out.println(solution.intToRoman2(1994));
 
     }
 
@@ -19,7 +19,6 @@ public class Solution {
         for (int i = 0; i < ints.length; i++) {
             tmp = num / ints[i];
             if (tmp > 0) {
-                System.out.println(tmp);
                 if ((i == 2 || i == 4 || i == 6) && tmp == 4) {
                     //sb.append(chars[i]).append(chars[i - 1]);
                     list.add(chars[i]);
@@ -40,6 +39,36 @@ public class Solution {
         }
         StringBuilder sb = new StringBuilder();
         list.forEach(sb::append);
+        return sb.toString();
+    }
+
+    public String intToRoman2(int num) {
+        int[] ints = new int[]{1000, 500, 100, 50, 10, 5, 1};
+        char[] chars = new char[]{'M', 'D', 'C', 'L', 'X', 'V', 'I'};
+        StringBuilder sb = new StringBuilder();
+        //ArrayList<Character> list = new ArrayList<>();
+        int tmp;
+        for (int i = 0; i < 7; i++) {
+            tmp = num / ints[i];
+            if (tmp > 0) {
+                if ((i == 2 || i == 4 || i == 6) && tmp == 4) {
+                    //sb.append(chars[i]).append(chars[i - 1]);
+                    sb.append(chars[i]);
+                    if (sb.length() >= 2 && sb.charAt(sb.length() - 2) == chars[i - 1]) {
+                        sb.delete(sb.length() - 2, sb.length() - 1);
+                        sb.append(chars[i - 2]);
+                    } else {
+                        sb.append(chars[i - 1]);
+                    }
+                } else {
+                    for (int j = 0; j < tmp; j++) {
+                        //sb.append(chars[i]);
+                        sb.append(chars[i]);
+                    }
+                }
+                num = num % ints[i];
+            }
+        }
         return sb.toString();
     }
 }
