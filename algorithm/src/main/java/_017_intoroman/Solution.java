7060d8733bf71c3191794f915df0587782df6800
YiJiuChow <19550518zrf@gmail.com>
2022-11-28 22:46:15 +0800

    罗马转int,细节String的遍历,不考虑空间复杂度的情况,toCharArray遍历比charAt效率高

--- algorithm/src/main/java/_017_intoroman/Solution.java
+++ algorithm/src/main/java/_017_intoroman/Solution.java
@@ -1,15 +1,21 @@
 package _017_intoroman;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.intToRoman2(1994));
+        String s = solution.intToRoman2(1994);
+        System.out.println(s);
+        int i = solution.romanToInt2(s);
+        System.out.println(i);
 
     }
 
+    //数字转罗马
     public String intToRoman(int num) {
         int[] ints = new int[]{1000, 500, 100, 50, 10, 5, 1};
         char[] chars = new char[]{'M', 'D', 'C', 'L', 'X', 'V', 'I'};
@@ -43,6 +49,7 @@ public class Solution {
     }
 
     /**
+     * 数字转罗马
      * 少用arraylist, 真的很慢
      * @param num
      * @return
@@ -76,5 +83,79 @@ public class Solution {
             }
         }
         return sb.toString();
+    }
+
+    /**
+     * 罗马转数字
+     * @param s
+     * @return
+     */
+    public int romanToInt(String s) {
+        int[] ints = new int[]{1000, 500, 100, 50, 10, 5, 1};
+        char[] chars = new char[]{'M', 'D', 'C', 'L', 'X', 'V', 'I'};
+        char[] ss = s.toCharArray(); // toCharArray遍历比charAt遍历效率高!!!
+        int[] res = new int[ss.length];
+        int i = 0, j = 0;
+        while (i < ss.length && j < 7) {
+            if (ss[i] == chars[j]) {
+                res[i] = ints[j];
+                if (i + 1 < ss.length && j >= 1) {
+                    if (ss[i + 1] == chars[j - 1]) {
+                        res[i] = -ints[j];
+                        res[i + 1] = ints[j - 1];
+                        i += 2;
+                        continue;
+                    } else if (j >= 2 && ss[i + 1] == chars[j - 2]) {
+                        res[i] = -ints[j];
+                        res[i + 1] = ints[j - 2];
+                        i += 2;
+                        continue;
+                    }
+                }
+                i++;
+            } else {
+                j++;
+            }
+        }
+        int result = 0;
+        for (int re : res) {
+            result += re;
+        }
+        return result;
+    }
+
+    public int romanToInt2(String s) {
+        int[] ints = new int[]{1000, 500, 100, 50, 10, 5, 1};
+        char[] chars = new char[]{'M', 'D', 'C', 'L', 'X', 'V', 'I'};
+        int[] res = new int[s.length()];
+        int i = 0, j = 0;
+        while (i < s.length() && j < 7) {
+            if (s.charAt(i) == chars[j]) {
+                res[i] = ints[j];
+                if (i + 1 < s.length() && j >= 1) {
+                    if (s.charAt(i + 1) == chars[j - 1]) {
+                        res[i] = -ints[j];
+                        res[i + 1] = ints[j - 1];
+                        //i++;
+                        i += 2;
+                        continue;
+                    } else if (j >= 2 && s.charAt(i + 1) == chars[j - 2]) {
+                        res[i] = -ints[j];
+                        res[i + 1] = ints[j - 2];
+                        //i++;
+                        i += 2;
+                        continue;
+                    }
+                }
+                i++;
+            } else {
+                j++;
+            }
+        }
+        int result = 0;
+        for (int re : res) {
+            result += re;
+        }
+        return result;
     }
 }
