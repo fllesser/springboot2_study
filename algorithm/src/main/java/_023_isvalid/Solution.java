a5d0f038bf8e488caa7bf103c495edebf17cf690
elastichow <19550518zrf@gmail.com>
2022-12-18 19:28:43 +0800

    经典算法, 有效括号

--- algorithm/src/main/java/_023_isvalid/Solution.java
+++ algorithm/src/main/java/_023_isvalid/Solution.java
@@ -1,8 +1,28 @@
 package _023_isvalid;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Stack;
 
 public class Solution {
-
+    public boolean isValid(String s) {
+        if (s.length() % 2 != 0) return false;
+        HashMap<Character, Character> map = new HashMap<>();
+        map.put(')', '(');
+        map.put(']', '[');
+        map.put('}', '{');
+        Stack<Character> stack = new Stack<>();
+        char temp;
+        for (int i = 0; i < s.length(); i++) {
+            temp = s.charAt(i);
+            if (!map.containsKey(temp)) {
+                stack.push(temp);
+            } else {
+                if (stack.isEmpty()) return false;
+                if (map.get(temp) != stack.pop()) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
 }
